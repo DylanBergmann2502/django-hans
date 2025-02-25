617f4fa0d34443af834382aa2a49268e7bbd67a4
Dylan <locducnguyen2001@gmail.com>
2025-02-25 15:23:59 +0700

    feat: install primevue

--- web/src/main.js
+++ web/src/main.js
@@ -3,6 +3,11 @@ import '@/assets/styles.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+
 import App from './App.vue';
 import router from './router';
 
@@ -10,5 +15,13 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  }
+});
+app.use(ToastService);
+app.use(ConfirmationService);
 
 app.mount('#app')
