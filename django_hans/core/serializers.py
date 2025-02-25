b1690f24174482d8b88c8caddc7f553766b7b094
Dylan <locducnguyen2001@gmail.com>
2025-02-25 11:10:03 +0700

    feat: create a simple view for health checking

---
+++ django_hans/core/serializers.py
@@ -0,0 +1,9 @@
+# django_hans/core/serializers.py
+from rest_framework import serializers
+
+
+class HealthCheckSerializer(serializers.Serializer):
+    status = serializers.CharField()
+    services = serializers.DictField(
+        child=serializers.CharField()
+    )
