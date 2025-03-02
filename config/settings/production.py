530eec613835598d954b9878f7f9e0f4149770ad
Dylan <locducnguyen2001@gmail.com>
2025-03-02 11:03:32 +0700

    fix: extend production allowed hosts to localhost

--- config/settings/production.py
+++ config/settings/production.py
@@ -20,7 +20,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["django-hans.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["django-hans.com", ".localhost", "127.0.0.1"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
