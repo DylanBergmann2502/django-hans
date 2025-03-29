4cbae2bf57bd180c989355e89f6974b507dadb54
Dylan <locducnguyen2001@gmail.com>
2025-03-29 10:26:01 +0700

    feat: install vitest

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,5 +1,7 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default [
@@ -13,7 +15,20 @@ export default [
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
 
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
+
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/__tests__/*'],
+  },
   skipFormatting,
 ]
