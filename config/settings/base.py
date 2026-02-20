7b2fcbacf58faa19b768d00f2a9e6bcb30381729
Dylan <locducnguyen2001@gmail.com>
2026-02-20 15:36:34 +0700

    fix: restore base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -50,6 +50,8 @@ DATABASES = {
         "ATOMIC_REQUESTS": True,
     }
 }
+# https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # URLS
 # ------------------------------------------------------------------------------
@@ -221,7 +223,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = ["noreply@gmail.com"]
+ADMINS = [("""Dylan Bergmann""", "noreply@gmail.com")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
